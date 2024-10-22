ea2c332f49867dd5c15dee06a18ba82367588db8
kabuto64425 <kabuto64425_higasumi52@yahoo.co.jp>
2024-10-22 17:07:25 +0900

    Update views.py

--- kanjomotocho/views.py
+++ kanjomotocho/views.py
@@ -15,6 +15,49 @@ logger = logging.getLogger(__name__)
 class KanjoMotochoView(AccountingPeriodLoginRequiredMixin, TemplateView):
     template_name = "kanjo_motocho/kanjo_motocho.html"
 
+    def shiwake_soneki(self):
+        period_from_queryparam = self.find_period_from_queryparam("last_day")
+        user = self.request.user  # ログインユーザーモデルの取得
+
+        filters = {
+            "shiwake__owner" : user,
+            "shiwake__shiwake_date__gte": period_from_queryparam[0],
+            "shiwake__shiwake_date__lte": period_from_queryparam[1],
+            "kanjo_kamoku__kamoku_type__in": [MasterKanjoKamokuType.HIYO, MasterKanjoKamokuType.SHUEKI]
+        }
+
+        qs = Kanjo.objects.select_related('shiwake').select_related('kanjo_kamoku').annotate(
+                # 借方なら+amount
+                # 貸方なら-amount
+                # を保存する列taishaku_amount
+                taishaku_amount = Case(
+                    When(taishaku=True, then=F("amount")),
+                    default=-F("amount")
+                )
+            ).filter(**filters).values(
+                #valuesを使用すると、クエリ実行結果はdict型のリストになる
+                "kanjo_kamoku", "kanjo_kamoku__name", "kanjo_kamoku__kamoku_type"
+            ).annotate(
+                # taishaku_amountを勘定科目ごとで集計した列zandaka
+        	    zandaka = Sum('taishaku_amount'),
+                name = F("kanjo_kamoku__name"),
+                kamoku_type = F("kanjo_kamoku__kamoku_type")
+            )
+        
+        hiyo_list = []
+        shueki_list = []
+
+        for kanjo in qs:
+            if kanjo["kamoku_type"] == MasterKanjoKamokuType.HIYO:
+                hiyo_list.append(kanjo)
+            elif kanjo["kamoku_type"] == MasterKanjoKamokuType.SHUEKI:
+                kanjo["zandaka"] *= -1
+                shueki_list.append(kanjo)
+        
+        hiyo_zandaka_sum = sum([kanjo["zandaka"] for kanjo in hiyo_list])
+        shueki_zandaka_sum = sum([kanjo["zandaka"] for kanjo in shueki_list])
+
+
     # 適用のリストを貸方、借方それぞれ作成。dict型で返す
     def build_tekiyo_list(self, target_kanjo_kamoku):
         if not target_kanjo_kamoku:
